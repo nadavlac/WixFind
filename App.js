79277f4e7683a69697018a765dfcbcb34673b101
talbe <talbe@wix.com>
2019-04-10 18:00:11 +0300

    worked mapppp!

--- App.js
+++ App.js
@@ -7,9 +7,10 @@
  */
 
 import React, {Component} from 'react';
-import {Platform, StyleSheet, Text, View} from 'react-native';
+import {Platform, StyleSheet, Text, View, TouchableOpacity, Linking, Image} from 'react-native';
 import MapView, { PROVIDER_GOOGLE } from 'react-native-maps';
-import { Marker } from 'react-native-maps';
+import { Marker, Callout } from 'react-native-maps';
+const businesses = require('./businessesData')
 
 
 const instructions = Platform.select({
@@ -21,19 +22,51 @@ const instructions = Platform.select({
 
 type Props = {};
 export default class App extends Component<Props> {
+
+
+
+  constructor(props){
+    super(props);
+
+    this.state = {
+
+    }
+    this.renderCallout = this.renderCallout.bind(this)
+  }
+
+  renderCallout(busi) {
+    return (
+      <TouchableOpacity onPress={() => Linking.openURL(busi.siteUrl)}>
+        <Text>{busi.name}</Text>
+        {/*<Image source={{uri: `https://static.wixstatic.com/media/${busi.logoUrl}`}} style={{width: 20, height:20}}/>*/}
+      </TouchableOpacity>
+    )
+  }
+
   render() {
     return (
       <View style={styles.container}>
          <MapView
-            // provider={PROVIDER_GOOGLE} // remove if not using Google Maps
-            style={styles.map}
-            region={{
-              latitude: 37.78825,
-              longitude: -122.4324,
-              latitudeDelta: 0.015,
-              longitudeDelta: 0.0121,
-            }}
-          />
+          // provider={PROVIDER_GOOGLE} // remove if not using Google Maps
+          style={styles.map}
+          // region={{
+          //   latitude: 37.78825,
+          //   longitude: -122.4324,
+          //   latitudeDelta: 0.015,
+          //   longitudeDelta: 0.0121,
+          // }}
+         >
+           {businesses.map((busi, i) => (
+             <Marker
+               key={i}
+               coordinate={{latitude: busi.latitude, longitude: busi.longitude}}
+             >
+               <Callout>
+                 {this.renderCallout(busi)}
+               </Callout>
+             </Marker>
+           ))}
+         </MapView>
       </View>
     );
   }
