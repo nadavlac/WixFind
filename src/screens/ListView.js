18f1b67e7a9855b5f4daeba9c8ee958d37301136
talbe <talbe@wix.com>
2019-04-11 12:43:36 +0300

    worked list , styles

--- src/screens/ListView.js
+++ src/screens/ListView.js
@@ -1,7 +1,7 @@
 import React, {Component} from 'react';
-import {StyleSheet, Alert, FlatList, TouchableOpacity, Image} from 'react-native';
+import {StyleSheet, Alert, FlatList, TouchableOpacity, Image, Linking} from 'react-native';
 import * as Animatable from 'react-native-animatable';
-import {AnimatableManager, ThemeManager, Colors, BorderRadiuses, ListItem, Text, View} from 'react-native-ui-lib'; //eslint-disable-line
+import {AnimatableManager, ThemeManager, Colors, BorderRadiuses, ListItem, Text, View, Button} from 'react-native-ui-lib'; //eslint-disable-line
 import MapScreen from './MapView';
 const mapIcon = require('../../assets/mapIcon.png');
 const listIcon = require('../../assets/listIcon.png');
@@ -18,7 +18,7 @@ export default class ListView extends Component {
     };
   }
 
-  keyExtractor = item => item.msId;
+  keyExtractor = (item,index) => index;
 
   renderRow(row, id) {
     const statusColor = /*row.inventory.status === 'Paid' ? Colors.green30 : */Colors.red30;
@@ -27,43 +27,47 @@ export default class ListView extends Component {
 
     return (
       <Animatable.View {...animationProps}>
-        <ListItem
-          activeBackgroundColor={Colors.dark60}
-          activeOpacity={0.3}
-          height={77.5}
-          onPress={() => Alert.alert(`pressed on order #${id + 1}`)}
-        >
-          <ListItem.Part left>
-            <Animatable.Image
-              source={{uri: `https://static.wixstatic.com/media/${row.logoUrl}`}}
-              style={styles.image}
-              {...imageAnimationProps}
-            />
-          </ListItem.Part>
-          <ListItem.Part middle column containerStyle={[styles.border, {paddingRight: 17}]}>
-            <ListItem.Part containerStyle={{marginBottom: 3}}>
-              <Text dark10 text70 style={{flex: 1, marginRight: 10}} numberOfLines={1}>{row.name}</Text>
-              <Text dark10 text70 style={{marginTop: 2}}>{row.email}</Text>
-            </ListItem.Part>
-            <ListItem.Part>
-              <Text style={{flex: 1, marginRight: 10}} text90 dark40 numberOfLines={1}>{`${row.shortDescription} item`}</Text>
-              <Text text90 color={statusColor} numberOfLines={1}>{row.phone}</Text>
-            </ListItem.Part>
-          </ListItem.Part>
-        </ListItem>
-      </Animatable.View>
+            <ListItem
+              activeBackgroundColor={Colors.dark60}
+              activeOpacity={0.3}
+              height={77.5}
+              onPress={() => Alert.alert(`pressed on order #${id + 1}`)}
+            >
+              <ListItem.Part left>
+                <Animatable.Image
+                  source={{uri: `https://static.wixstatic.com/media/${row.imageUrl ? row.imageUrl : row.business.logoUrl}`}}
+                  style={styles.image}
+                  {...imageAnimationProps}
+                />
+              </ListItem.Part>
+              <ListItem.Part middle column containerStyle={[styles.border, {paddingRight: 17}]}>
+                <Text dark10 text70 style={{flex: 1, marginRight: 10,}} numberOfLines={1}>{row.name}</Text>
+                <Text dark10 text50 style={{flex: 1, marginRight: 10}} numberOfLines={2}>{row.business.name}</Text>
+              </ListItem.Part>
+              <ListItem.Part right>
+                <Button label={'Book'}
+                        labelStyle={{fontWeight: '100'}}
+                        size='small'
+                        text90
+                        style={{marginBottom: 20}}
+                        onPress={() => Linking.openURL(row.url)}
+                />
+              </ListItem.Part>
+            </ListItem>
+        </Animatable.View>
+
     );
   }
 
   renderList() {
-    const {businesses, query} = this.props;
+    const {services, query} = this.props;
     return (
       <View>
         <Text  color={Colors.red10} text50 middle style={{marginTop: 10, marginLeft: 65}}>{`you're looking for ${query}`}</Text>
         <FlatList
-          data={businesses}
+          data={services}
           renderItem={({item, index}) => this.renderRow(item, index)}
-          keyExtractor={this.keyExtractor}
+          keyExtractor={(item, index) => index.toString()}
         />
       </View>
     )
